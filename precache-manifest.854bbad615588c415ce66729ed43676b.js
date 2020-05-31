cec21be72769c639a3508e309ef21b95c04c1700
Saad Tahmid <saadtahmid1890@gmail.com>
2020-05-31 17:29:14 +0600

    Updates

---
+++ precache-manifest.854bbad615588c415ce66729ed43676b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddf767bd4ef1f075666cc73e1237933",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "38754fd28ca54cc4b355",
+    "url": "/calculator/static/css/main.59dc10f2.chunk.css"
+  },
+  {
+    "revision": "360ef2d2c651025bb267",
+    "url": "/calculator/static/js/2.803aaea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.803aaea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38754fd28ca54cc4b355",
+    "url": "/calculator/static/js/main.945a1dcf.chunk.js"
+  },
+  {
+    "revision": "6a59245152ba12401b70",
+    "url": "/calculator/static/js/runtime-main.814ed3cc.js"
+  }
+]);
